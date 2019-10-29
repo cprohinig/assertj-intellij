e81bd16f0827f2161ebcaff2c5eae7e73f47266f
Christian Prohinig <christian.prohinig@dynatrace.com>
2019-10-29 17:16:00 +0100

    add creation of assert class and directories

--- src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
+++ src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
@@ -5,11 +5,18 @@ import com.intellij.lang.jvm.JvmClassKind;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.impl.PsiManagerImpl;
+import com.intellij.psi.impl.file.PsiDirectoryImpl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.StringTokenizer;
 
 public class AssertionGeneratorActions extends AnAction {
 
@@ -19,6 +26,7 @@ public class AssertionGeneratorActions extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
         final Project project = e.getProject();
+
         final PsiJavaFile javaFile = getActiveJavaFile(editor, project);
         if (javaFile != null) {
             final AssertionDetailsGenerator assertionDetailsGenerator = new AssertionDetailsGeneratorImpl();
@@ -26,7 +34,43 @@ public class AssertionGeneratorActions extends AnAction {
             final String assertionContents = assertionGenerator.generateContent(javaFile);
             final PsiFileFactory factory = PsiFileFactory.getInstance(project);
 
-            factory.createFileFromText(assertionGenerator.generateFilename(javaFile), JavaFileType.INSTANCE, assertionContents);
+            final PsiFile file = factory.createFileFromText(assertionGenerator.generateFilename(javaFile), JavaFileType.INSTANCE, assertionContents);
+
+//            final PsiFile file = factory.createFileFromText("TestAssertion.java", JavaFileType.INSTANCE, "package com.dynatrace.cloud.simulators.infrastructure.components;import com.dynatrace.cloud.simulators.infrastracture.Node; public class Account implements Node { private final String id;public Account(String id) {this.id = id;}@Override public String getId() { return id; }}");
+            final PsiDirectory parent = javaFile.getParent();
+            final PsiManagerImpl psiManager = (PsiManagerImpl)PsiManager.getInstance(project);
+            final PsiDirectory root = new PsiDirectoryImpl(psiManager, project.getBaseDir());
+            PsiDirectory dir = root;
+
+            if (parent != null) {
+                String dirName = parent.getVirtualFile().getPresentableUrl();
+                StringTokenizer t = new StringTokenizer(dirName, "/", false);
+                boolean rootFound = false;
+                while(t.hasMoreElements()) {
+                    String subDir = (String) t.nextElement();
+                    subDir = subDir.equals("main") ? "test" : subDir;
+
+                    if(rootFound) {
+                        if(dir.findSubdirectory(subDir) == null) {
+                            dir = dir.createSubdirectory(subDir);
+                        } else {
+                            dir = dir.findSubdirectory(subDir);
+                        }
+                    }
+                    if(subDir.equals(root.getName())) {
+                        rootFound = true;
+                    }
+                }
+
+                if(dir != null) {
+                    JavaCodeStyleManager.getInstance(project).optimizeImports(file);
+                    CodeStyleManager.getInstance(project).reformat(file);
+                    PsiDirectory finalDir = dir;
+                    Runnable addFileOperation = ()-> finalDir.add(file);
+                    WriteCommandAction.runWriteCommandAction(project, addFileOperation);
+                    dir.findFile(assertionGenerator.generateFilename(javaFile)).navigate(true);
+                }
+            }
         }
     }
 
