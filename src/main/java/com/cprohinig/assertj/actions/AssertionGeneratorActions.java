21d3e008dc2fcd182af30b86356b7b850ea7b747
Christian Prohinig <christian.prohinig@dynatrace.com>
2019-10-29 09:32:25 +0100

    switch to real dependency of assertj generator

--- src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
+++ src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
@@ -2,12 +2,33 @@ package com.cprohinig.assertj.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.PsiDocumentManagerImpl;
+import org.assertj.assertions.generator.BaseAssertionGenerator;
+import org.assertj.assertions.generator.description.ClassDescription;
+import org.assertj.assertions.generator.description.converter.ClassToClassDescriptionConverter;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class AssertionGeneratorActions extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        System.out.println("Test");
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+
+        final PsiFile psiFile = PsiDocumentManager.getInstance(e.getProject()).getPsiFile(editor.getDocument());
+        System.out.println("test");
+//        try {
+//            BaseAssertionGenerator baseAssertionGenerator = new BaseAssertionGenerator();
+//            ClassToClassDescriptionConverter converter = new ClassToClassDescriptionConverter();
+//            ClassDescription classDescription = converter.convertToClassDescription();
+//            baseAssertionGenerator.generateCustomAssertionFor(classDescription);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
     }
 }
