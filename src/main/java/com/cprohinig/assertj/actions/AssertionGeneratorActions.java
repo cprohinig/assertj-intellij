38e31a249661c0c9b8f8c6a09f8ce264e32ad4dc
Christian Prohinig <christian.prohinig@dynatrace.com>
2019-10-29 14:11:05 +0100

    only show entry in context menu when opened from java class

--- src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
+++ src/main/java/com/cprohinig/assertj/actions/AssertionGeneratorActions.java
@@ -1,28 +1,44 @@
 package com.cprohinig.assertj.actions;
 
+import com.intellij.lang.jvm.JvmClassKind;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiJavaFile;
 import org.jetbrains.annotations.NotNull;
 
 public class AssertionGeneratorActions extends AnAction {
 
+    private static final String JAVA_FILE_EXT = "java";
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        final Editor editor = e.getData(CommonDataKeys.EDITOR);
 
-        final PsiFile psiFile = PsiDocumentManager.getInstance(e.getProject()).getPsiFile(editor.getDocument());
-        System.out.println("test");
-//        try {
-//            BaseAssertionGenerator baseAssertionGenerator = new BaseAssertionGenerator();
-//            ClassToClassDescriptionConverter converter = new ClassToClassDescriptionConverter();
-//            ClassDescription classDescription = converter.convertToClassDescription();
-//            baseAssertionGenerator.generateCustomAssertionFor(classDescription);
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent anActionEvent) {
+        boolean visible = false;
+        final Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
+        final Project project = anActionEvent.getProject();
+
+        if (editor != null && project != null) {
+            final PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+            if (file != null && JAVA_FILE_EXT.equals(file.getVirtualFile().getExtension())) {
+                final PsiJavaFile javaFile = (PsiJavaFile) file;
+                for (PsiClass clazz : javaFile.getClasses()) {
+                    if (JvmClassKind.CLASS.equals(clazz.getClassKind())) {
+                        visible = true;
+                    }
+                }
+            }
+        }
+
+        anActionEvent.getPresentation().setVisible(visible);
     }
 }
